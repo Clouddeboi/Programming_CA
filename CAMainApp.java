8ca2d914b807b6aaec39ecf09970aa5a1fb12aa9
Cloud <michalcloud2003@gmail.com>
2023-11-05 18:45:25 +0000

    Made it so i can read the file
    I used a yt video to code in the file reader and will comment what each line does in the code and will reformat the code later for my specific needs.

--- CAMainApp.java
+++ CAMainApp.java
@@ -1,5 +1,48 @@
 package Programming_CA.Programming_CA;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class CAMainApp {
+    public static void main(String[] args) {
+
+        String file = "src\\activity_data_50.csv";
+
+        BufferedReader reader = null;
+
+        String line = "";
+
+        try
+        {
+            reader = new BufferedReader(new FileReader(file));
+            while((line = reader.readLine()) != null)
+            {
+                String[] row = line.split(",");
+
+                for(String index : row)
+                {
+                    System.out.printf("%-10s", index);
+                }
+                System.out.println();
+            }
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+        finally
+        {
+            try
+            {
+                reader.close();
+            }
+            catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+
+        }
+    }
 
 }
