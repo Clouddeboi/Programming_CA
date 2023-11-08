b291bbeee8b03449a20a7f49fce4a17be5547e39
ganyuvu <145359177+ganyuvu@users.noreply.github.com>
2023-11-08 14:16:51 +0000

    Added Date Comparator

--- DateComparator.java
+++ DateComparator.java
@@ -1,11 +1,34 @@
 package Programming_CA.Programming_CA;
 
-import java.util.Comparator;
-
 public class DateComparator implements Comparator<Activity> {
-        @Override
-        public int compare(Activity a1, Activity a2) {
-            return Double.compare(a1.getDistance(), a2.getDistance());
+    @Override
+    public int compare(Activity a1, Activity a2) {
+
+        int a1Date[] = parseDate(a1.getDate());
+        int a2Date[] = parseDate(a2.getDate());
+
+        if(a1Date[2] == a2Date[2])
+        {
+            if(a1Date[1] == a2Date[1])
+            {
+                return a1Date[0] - a2Date[0];
+            }
+            return a1Date[1] - a2Date[1];
         }
+        return a1Date[2] - a2Date[2];
     }
+
+    public int[] parseDate(String s)
+    {
+        int[] d = new int[3];
+        String[] date = s.split("/");
+        for(int i =0; i < date.length;i++)
+        {
+            d[i] = Integer.parseInt(date[i].trim());
+        }
+        return d;
+    }
+}
+
+
 
